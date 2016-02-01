451b2c278bf0a3714be33100ae1ac6b14cdc488d
Oscar Wei <oscarwei168@msn.com>
2016-02-01 21:33:27 +0800

    More google guava cache examples

--- src/main/java/tw/com/oscar/guava/cache/CacheTest.java
+++ src/main/java/tw/com/oscar/guava/cache/CacheTest.java
@@ -14,11 +14,13 @@
 package tw.com.oscar.guava.cache;
 
 import com.google.common.cache.*;
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableFutureTask;
 import tw.com.oscar.orm.hibernate.domain.Account;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * <strong>Description:</strong><br>
@@ -30,6 +32,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class CacheTest {
 
+    final static ExecutorService executor = Executors.newCachedThreadPool();
+
     public static void main(String[] args) {
         Account account = null;
         LoadingCache<String, Account> loadingCache = CacheBuilder.newBuilder()
@@ -39,19 +43,36 @@ public class CacheTest {
                         return value.getRoles().size();
                     }
                 }).expireAfterWrite(10, TimeUnit.MINUTES)
-                .removalListener(new RemovalListener<Object, Object>() {
+                .removalListener(new RemovalListener<String, Account>() {
                     @Override
-                    public void onRemoval(RemovalNotification<Object, Object> removalNotification) {
+                    public void onRemoval(RemovalNotification<String, Account> removalNotification) {
+                        Account account = removalNotification.getValue();
                         // TODO
                     }
-                })
+                }).refreshAfterWrite(1, TimeUnit.MINUTES)
                 .build(new CacheLoader<String, Account>() {
                     @Override
                     public Account load(String s) throws Exception {
                         return null;
                     }
+
+                    @Override
+                    public ListenableFuture<Account> reload(final String key, Account account) {
+                        if (true) {
+                            return Futures.immediateFuture(account);
+                        } else {
+                            ListenableFutureTask<Account> task = ListenableFutureTask.create(new Callable<Account>() {
+                                public Account call() {
+                                    return null;
+                                }
+                            });
+                            executor.execute(task);
+                            return task;
+                        }
+                    }
                 });
         // ....
+        CacheStats cacheStats = loadingCache.stats();
         try {
             account = loadingCache.get("aid");
             // ImmutableMap<String, Account> accountList = loadingCache.getAll(Lists.newArrayList("", ""));
@@ -76,5 +97,10 @@ public class CacheTest {
 
         // inserted directly
         loadingCache1.put("aid1", account);
+
+        // explicit removals
+        loadingCache.invalidate("");
+        loadingCache.invalidateAll(Lists.newArrayList("", ""));
+
     }
 }
